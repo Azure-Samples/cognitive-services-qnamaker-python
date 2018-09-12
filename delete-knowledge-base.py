20ce14da67f243c2747922bae5cab447d99314f4
Winona Azure <38537084+wiazur@users.noreply.github.com>
2018-09-12 13:59:08 -0700

    Added delete knowledge base file

---
+++ delete-knowledge-base.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, json, time
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# Replace this with a valid subscription key.
+subscriptionKey = 'ADD KEY HERE'
+
+# Replace this with a valid knowledge base ID, the one you want to delete.
+kb = 'ADD KNOWLEDGE BASE ID HERE';
+
+host = 'westus.api.cognitive.microsoft.com'
+service = '/qnamaker/v4.0'
+method = '/knowledgebases/'
+
+def pretty_print (content):
+# Note: We convert content to and from an object so we can pretty-print it.
+    return json.dumps(json.loads(content), indent=4)
+
+def delete_kb (path, content):
+    print ('Calling ' + host + path + '.')
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+        'Content-Type': 'application/json',
+        'Content-Length': len (content)
+    }
+    conn = http.client.HTTPSConnection(host)
+    conn.request ("DELETE", path, content, headers)
+    response = conn.getresponse ()
+
+    if response.status == 204:
+        return json.dumps({'result' : 'Success.'})
+    else:
+        return response.read ()
+
+path = service + method + kb
+result = delete_kb (path, '')
+print (pretty_print(result))
